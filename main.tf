5a6d4e27696bbbca1b57602b737581094768673c
raviteja <ravitejatfcdemo@gmail.com>
2023-02-16 10:53:54 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-#module "subnets" {
-#  source = "./subnets"
-#  availability_zone = var.availability_zone
-#  cidr_block = var.cidr_block
-#  default_vpc_id = var.default_vpc_id
-#  env = var.env
-#  name = var.name
-#  vpc_id = var.vpc_id
-#  vpc_peering_connection_id = var.vpc_peering_connection_id
-#}
+module "subnets" {
+  source = "./subnets"
+  availability_zone = var.availability_zone
+  cidr_block = var.cidr_block
+  default_vpc_id = var.default_vpc_id
+  env = var.env
+  name = var.name
+  vpc_id = var.vpc_id
+  vpc_peering_connection_id = var.vpc_peering_connection_id
+}
 
 #resource "aws_route" "internet_gw_route" {
 #  count = var.internet_gw ? 1 : 0
